d97f9a55a706aa26f2626b617724e32196c8f716
Ramón <ramon.mosquera@gmail.com>
2021-11-20 20:54:30 +0100

    Index and property og (social media) metatags

--- frontend/views/layouts/mainbrooks.php
+++ frontend/views/layouts/mainbrooks.php
@@ -19,6 +19,16 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="robots" content="index, follow">
+    
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Clínica de medicina estética Doctora Mosquera">
+    <meta property="og:description" content="Clínica de medicina estética en Ferrol.">
+    <meta property="og:url" content="http://www.clinicadramosquera.com/">
+    <meta property="og:site_name" content="Clínica Doctora Mosquera">
+    
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
